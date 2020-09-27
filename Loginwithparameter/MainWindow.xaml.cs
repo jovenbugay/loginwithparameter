726ec592c7087267fe3e1f12e681c00fdced26c5
jovenbugay <jovenbugay@gmail.com>
2020-09-27 16:51:13 +0800

    update upto 09.27.2020

---
+++ Loginwithparameter/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Loginwithparameter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            String name = txtusername.Text;
+            String password = txtpassword.Password;
+            if (txtpassword.Password == "123" && txtusername.Text == "Joven")
+            {
+                Window win1 = new Window1(name, password);
+                win1.Show();
+               
+
+            }
+            else
+            {
+                Status.Content = "Error logging in";
+            }
+
+
+
+        }
+    }
+}
